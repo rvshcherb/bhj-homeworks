f44962302193285a81edfaad55e010aa6756569a
rvshcherb <shcherbinin_rv@mail.ru>
2019-06-28 15:44:18 +0300

    second commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -18,29 +18,6 @@ setInterval(switchPhrase, 1000);
 
 
 
-/******** Решение ч/з массив ********/
-
-// const phrases = Array.from(document.querySelectorAll('.rotator__case'));
-// let i = 0;
-
-// function clearClass() {
-//   for(phrase of phrases) {
-//     phrase.classList.remove('rotator__case_active');
-//   }
-// }
-
-// function switchPhrase() {
-//   clearClass();
-//   phrases[i].classList.add('rotator__case_active')
-//   i++;
-//   if (i > phrases.length - 1) {
-//     i = 0;
-//   }
-// }
-
-// setInterval(switchPhrase, 1000);
-
-
 /******** Решение ч/з навигацию по элементам с изменяемой скоростью ********/
 
 // let phrase = document.querySelector('.rotator__case');
--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,77 +1,36 @@
 const text = document.querySelector('.book');
 
-const sizes = document.querySelectorAll('.font-size');
+
 const sizeControl = document.querySelector('.book__control_font-size');
-
-const colors = document.querySelectorAll('.book__control.book__control_color .color');
 const colorControl = document.querySelector('.book__control_color');
-
-const backgrounds = document.querySelectorAll('.book__control.book__control_background .color');
 const backgroundControl = document.querySelector('.book__control_background');
 
 
-sizeControl.addEventListener('click', function(event) { 
-  for (size of sizes) {
-    event.preventDefault();
-    size.classList.remove('font-size_active');
-  }
-  event.target.classList.add('font-size_active');
 
-  switch(event.target.dataset.size) {
-    case 'small':
-    text.classList.add('book_fs-small');
-    text.classList.remove('book_fs-big');
-    break;
-    case 'big':
-    text.classList.add('book_fs-big');
-    text.classList.remove('book_fs-small');
-    break;
-    default:
-    text.classList.remove('book_fs-big', 'book_fs-small');
-  }
+sizeControl.addEventListener('click', function(event) { 
+  event.preventDefault();
+  this.querySelector('.font-size_active').classList.remove('font-size_active');
+  event.target.classList.add('font-size_active');
+  text.classList.remove('book_fs-small', 'book_fs-big');
+  text.classList.add(`book_fs-${event.target.dataset.size}`);
 })
 
 colorControl.addEventListener('click', function(event) { 
-  for (color of colors) {
-    event.preventDefault();
-    color.classList.remove('color_active');
-  }
+  event.preventDefault();
+  this.querySelector('.color_active').classList.remove('color_active');
   event.target.classList.add('color_active');
-
-  switch(event.target.dataset.color) {
-    case 'gray':
-    text.classList.add('book_color-gray');
-    text.classList.remove('book_color-whitesmoke');
-    break;
-    case 'whitesmoke':
-    text.classList.add('book_color-whitesmoke');
-    text.classList.remove('book_color-gray');
-    break;
-    default:
-    text.classList.remove('book_color-gray', 'book_color-whitesmoke');
-  }
+  text.classList.remove('book_color-gray', 'book_color-whitesmoke');
+  text.classList.add(`book_color-${event.target.dataset.color}`);
 })
 
 backgroundControl.addEventListener('click', function(event) { 
-  for (background of backgrounds) {
-    event.preventDefault();
-    background.classList.remove('color_active');
-  }
+  event.preventDefault();
+  this.querySelector('.color_active').classList.remove('color_active');
   event.target.classList.add('color_active');
-
-  switch(event.target.dataset.color) {
-    case 'gray':
-    text.classList.add('color_gray');
-    text.classList.remove('color_black');
-    break;
-    case 'black':
-    text.classList.add('color_black');
-    text.classList.remove('color_gray');
-    break;
-    default:
-    text.classList.remove('color_gray', 'color_black');
-  }
+  text.classList.remove('color-gray', 'color-black');
+  text.classList.add(`color_${event.target.dataset.color}`);
 })
+
 
 
 
