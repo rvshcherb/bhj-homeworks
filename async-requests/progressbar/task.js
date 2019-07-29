b4d40a2f9f26ae4719c40b3ff218d8385d46af34
rvshcherb <shcherbinin_rv@mail.ru>
2019-07-29 21:27:35 +0300

    second commit

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,18 +1,21 @@
 const progressBar = document.getElementById('progress');
 const form = document.getElementById('form');
 
+function addListeners(xhr) {
+  xhr.upload.addEventListener('loadstart', () => console.log('Loading started'));
+  xhr.upload.addEventListener('progress', (event) => console.log(event.loaded / event.total));
+  xhr.upload.addEventListener('progress', (event) => progressBar.value = (event.loaded / event.total).toFixed(1));
+  xhr.upload.addEventListener('load', () => console.log('Loading complete'));
+  xhr.upload.addEventListener('loadend', () => console.log('Request has been completed'));
+  xhr.upload.addEventListener('error', () => console.log('Error has occured'));
+}
+
 form.addEventListener('submit', function () {
   event.preventDefault();
-  let input = form.elements.file;
-  let file = input.files[0];
+  const formData = new FormData(form);
   const xhr = new XMLHttpRequest();
 
-  xhr.upload.addEventListener('progress', function (event) {
-    progressBar.value = (event.loaded / event.total).toFixed(1);
-  });
-
   xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
-  xhr.send(file);
-
-  
-});
+  addListeners(xhr);
+  xhr.send(formData);
+}); 
