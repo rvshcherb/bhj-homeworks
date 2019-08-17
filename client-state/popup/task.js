1c527cd2d6caa5dd1e7816bfe449918567b66e71
rvshcherb <shcherbinin_rv@mail.ru>
2019-08-17 23:15:53 +0300

    Second commit

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,13 +1,14 @@
 const modal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
+if(Cookies.get('modalShown')){
+  modal.classList.remove('modal_active');
+}
 
 modalClose.addEventListener('click', () => {
   modal.classList.remove('modal_active');
   document.cookie = "modalShown=true";
 });
 
-document.addEventListener('DOMContentLoaded', () => document.cookie = "test=1; max-age=3600");  
 
-Cookies.set('name', 'vasya');
 
